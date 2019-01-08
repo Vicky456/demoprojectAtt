4675142857eb9c1cc7a842ca592c7be852ad9f7f
vignesh <vigneshaprogramer@gmail.com>
2019-01-08 11:22:50 +0530

    fix db connection

--- app/src/main/java/com/priyamano/vickyattendance/ConnectionManager.java
+++ app/src/main/java/com/priyamano/vickyattendance/ConnectionManager.java
@@ -13,12 +13,15 @@ import java.sql.SQLException;
 public class ConnectionManager {
     private static Connection con;
 
+
     public static Connection getConnection() {
+        String _Server="118.91.234.206:1433",_database="MCET",_user="Faculty",_pass="Facultty@123";
         try {
             StrictMode.setThreadPolicy(new Builder().permitAll().build());
             Class.forName("net.sourceforge.jtds.jdbc.Driver");
             try {
 
+                //con = DriverManager.getConnection("jdbc:jtds:sqlserver://"+_Server+";database="+_database+";user="+_user+";password="+_pass+";encrypt=false;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
                 con = DriverManager.getConnection("jdbc:jtds:sqlserver://" + _Server + ";databaseName=" + _database + ";user=" + _user + ";password=" + _pass + ";");
             } catch (SQLException e) {
 
